703d31503cbbb0a19790ff3de6a0c84da00c25ad
Jay Jagpal <jay@strangemother.com>
2022-02-07 22:34:22 +0000

    Promoted custom functionality from the test area

--- porthouse/host.py
+++ porthouse/host.py
@@ -7,30 +7,95 @@ from fastapi.responses import HTMLResponse
 
 from .ingress import app
 from .house import connection
-from . import home
+from . import home, state
 
 
-con_manager = connection.Manager(app)
 
-# import scene
 
-# PORT = 8000
-# DEVICES = (
-#         scene.SceneManager,
-#         manager.BroadcastDevice,
-#     )
+class Forever(state.MicroState):
+    async def concurrent(self, data, owner, micro_position):
+        print('F:', data)
+        move_on = data.get('text', '').lower() == 'thanks'
+        return move_on, True
 
-# host = manager.Host(devices=DEVICES)
+
+class Debug(state.KeyMicroState):
+
+    name = 'debug'
+
+    async def push_state_message(self, micro_position, data, owner):
+        move_on = False
+        micro_val = 2
+        import pdb; pdb.set_trace()  # breakpoint 72df0baa //
+
+        return move_on, micro_val
+
+
+class HotMove(state.KeyMicroState):
+
+    async def push_state_message(self, micro_position, data, owner):
+        t = data.get('text', None)
+        if t == self.kwargs.get('text'):
+            self.release()
+        # move_on, micro_position inc
+        return False, True
+
+
+class EmitRelease(state.KeyMicroState):
+    async def push_state_message(self, micro_position, data, owner):
+        if (micro_position or 0) > 2:
+            raise state.Release()
+        return False, True
+
+
+# These plugins define the procedural list a single client should
+# walk. Each step is like a function - but built with an async waiting.
+plugins = (
+    state.MicroState(name='BEFORE'),
+    state.KeyMicroState(name='beta', move_to='other', init_value=-3),
+    state.KeyLobby(name='keyarea',
+        # state_index=0,
+        acceptors=(
+            state.MicroState(name='egg'),
+            HotMove(name='fred', text='egg', move_to='cake'),
+            state.KeyMicroState(name='beta', move_to='delta', init_value=-3),
+            state.MicroState(name='charlie'),
+            state.KeyMicroState(name='delta', move_to='charlie'),
+            state.KeyMicroState(name='cake', move_to='debug'),
+            Debug(),
+        )
+     ),
+    state.KeyMicroState(name='other', move_to='keyarea', init_value=1),
+    state.Lobby(name='area',
+        # state_index=0,
+        acceptors=(
+            Forever(name='FOREVER'),
+        )
+     ),
+)
+
+
+state_machine = state.StateMachine(plugins,
+        entry_acceptors=(
+            EmitRelease(name='EmitRelease'),
+            # state.MicroState(name='alpha'),
+            # state.MicroState(name='beta'),
+        )
+    )
+
+
+# state_machine = state.MicroMachine()
+
+con_manager = connection.Manager(state_machine)
+
+globals()['app'] = app
+
 
 @app.on_event("startup")
 async def startup_event():
     """Run the host manager within the async for await tools"""
     await con_manager.mount()
 
-
-globals()['app'] = app
-
-# from ingress import *
 
 @app.get("/")
 async def get():
